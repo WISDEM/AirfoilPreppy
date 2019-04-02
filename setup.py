6a38a0806f48adcf681e5506245dfc078ac9c6e8
gbarter <garrett.barter@nrel.gov>
2019-04-02 06:14:02 -0600

    simplified for easier listing as a dependency

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ from setuptools import setup  # , find_packages
 
 
 setup(
-    name='AirfoilPrep.py',
+    name='AirfoilPrep',
     version='0.1.0',
     description='Airfoil preprocessing for wind turbine applications',
     author='S. Andrew Ning',
     author_email='andrew.ning@nrel.gov',
     package_dir={'': 'src'},
-    package_data={'airfoilprep': []},
     packages=['airfoilprep'],
-    py_modules=['airfoilprep'],
     license='Apache License, Version 2.0',
     zip_safe=False
 )
